b8010b4a09255f7ab1eee64ecc3423ca5cefb9c3
Hiran <134024847+hiranKaushalR@users.noreply.github.com>
2024-04-06 02:45:44 +0530

    commit 4

--- JavaScript/inventory-add.js
+++ JavaScript/inventory-add.js
@@ -1,11 +1,23 @@
 new gridjs.Grid({
     search: true,
-    columns: ["Name", "Email", "Phone Number"],
+    pagination: {
+      limit: 8,
+      summary: false
+    },
+    columns: [{name: 'Index', sort: true}, {name: "Name", sort: true }, {name: "Buying Price", sort: true},  {name: "Category", sort: true},  {name: "Quantity", sort: true},  {name: "Quickly", sort: true},  {name: "Weight", sort: true}],
     data: [
-      ["John", "john@example.com", "(353) 01 222 3333"],
-      ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-      ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-      ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-      ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+      [1, 'Golden Bracelet', '40 000', 'Bracelet', 24, '18K', '175 g'],
+      [2, 'Golden Ring', '26 000', 'Ring', 21, '24K', '145 g'],
+      [3, 'Golden Neckless', '30 000', 'Neckless', 21, '20K', '180 g'],
+      [4, 'Golden Earring', '55 000', 'Earring', 28, '28K', '125 g'],
+      [1, 'Golden Bracelet', '40 000', 'Bracelet', 24, '18K', '175 g'],
+      [2, 'Golden Ring', '26 000', 'Ring', 21, '24K', '145 g'],
+      [3, 'Golden Neckless', '30 000', 'Neckless', 21, '20K', '180 g'],
+      [4, 'Golden Earring', '55 000', 'Earring', 28, '28K', '125 g'],
+      [2, 'Golden Ring', '26 000', 'Ring', 21, '24K', '145 g'],
+      [3, 'Golden Neckless', '30 000', 'Neckless', 21, '20K', '180 g'],
+      [1, 'Golden Bracelet', '40 000', 'Bracelet', 24, '18K', '175 g'],
+      [3, 'Golden Neckless', '30 000', 'Neckless', 21, '20K', '180 g'],
     ]
   }).render(document.getElementById("supply-detail-table"));
+  
