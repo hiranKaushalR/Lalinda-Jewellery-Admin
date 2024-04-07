004e1888de86da17d662b182b6f1167186e6a80b
Hiran <134024847+hiranKaushalR@users.noreply.github.com>
2024-04-07 19:33:57 +0530

    created the sales page

--- JavaScript/sales.js
+++ JavaScript/sales.js
@@ -1,3 +1,5 @@
+// TODO: Optimize the code by using functions
+
 // Getting ID For main section
 const all = document.getElementById("all");
 const colombo = document.getElementById("colombo");
@@ -147,6 +149,7 @@ necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
 braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
 earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
 ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+allProductsDetailContainerInAllSection.style.display = "block";
 necklaceProductsDetailContainerInAllSection.style.display = "none";
 braceletsProductsDetailContainerInAllSection.style.display = "none";
 earringsProductsDetailContainerInAllSection.style.display = "none";
@@ -175,6 +178,7 @@ all.addEventListener("click", () => {
   braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
   earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
   ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAllSection.style.display = "block";
   necklaceProductsDetailContainerInAllSection.style.display = "none";
   braceletsProductsDetailContainerInAllSection.style.display = "none";
   earringsProductsDetailContainerInAllSection.style.display = "none";
@@ -204,6 +208,7 @@ colombo.addEventListener("click", () => {
   braceletsProductsInColomboSection.style.backgroundColor = "#00B8F0";
   earringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
   ringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInColomboSection.style.display = "block";
   necklaceProductsDetailContainerInColomboSection.style.display = "none";
   braceletsProductsDetailContainerInColomboSection.style.display = "none";
   earringsProductsDetailContainerInColomboSection.style.display = "none";
@@ -233,6 +238,7 @@ ampara.addEventListener("click", () => {
   braceletsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
   earringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
   ringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAmparaSection.style.display = "block";
   necklaceProductsDetailContainerInAmparaSection.style.display = "none";
   braceletsProductsDetailContainerInAmparaSection.style.display = "none";
   earringsProductsDetailContainerInAmparaSection.style.display = "none";
@@ -240,21 +246,309 @@ ampara.addEventListener("click", () => {
 });
 
 // Sub Filter Bar
+// All Section
 // All tab in all
-allProductsInAllSection.addEventListener ("click", () => {
-    allProductsInAllSection.style.color = "#00B8F0";
-    necklaceProductsInAllSection.style.color = "#D9D9D9";
-    braceletsProductsInAllSection.style.color = "#D9D9D9";
-    earringsProductsInAllSection.style.color = "#D9D9D9";
-    ringsProductsInAllSection.style.color = "#D9D9D9";
-  
-    allProductsInAllSection.style.backgroundColor = "#D9D9D9";
-    necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
-    braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
-    earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
-    ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
-    necklaceProductsDetailContainerInAllSection.style.display = "none";
-    braceletsProductsDetailContainerInAllSection.style.display = "none";
-    earringsProductsDetailContainerInAllSection.style.display = "none";
-    ringsProductsDetailContainerInAllSection.style.display = "none";
-})
+allProductsInAllSection.addEventListener("click", () => {
+  allProductsInAllSection.style.color = "#00B8F0";
+  necklaceProductsInAllSection.style.color = "#D9D9D9";
+  braceletsProductsInAllSection.style.color = "#D9D9D9";
+  earringsProductsInAllSection.style.color = "#D9D9D9";
+  ringsProductsInAllSection.style.color = "#D9D9D9";
+
+  allProductsInAllSection.style.backgroundColor = "#D9D9D9";
+  necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAllSection.style.display = "block";
+  necklaceProductsDetailContainerInAllSection.style.display = "none";
+  braceletsProductsDetailContainerInAllSection.style.display = "none";
+  earringsProductsDetailContainerInAllSection.style.display = "none";
+  ringsProductsDetailContainerInAllSection.style.display = "none";
+});
+
+// Necklace tab in all
+necklaceProductsInAllSection.addEventListener("click", () => {
+  allProductsInAllSection.style.color = "#D9D9D9";
+  necklaceProductsInAllSection.style.color = "#00B8F0";
+  braceletsProductsInAllSection.style.color = "#D9D9D9";
+  earringsProductsInAllSection.style.color = "#D9D9D9";
+  ringsProductsInAllSection.style.color = "#D9D9D9";
+
+  allProductsInAllSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAllSection.style.backgroundColor = "#D9D9D9";
+  braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAllSection.style.display = "none";
+  necklaceProductsDetailContainerInAllSection.style.display = "block";
+  braceletsProductsDetailContainerInAllSection.style.display = "none";
+  earringsProductsDetailContainerInAllSection.style.display = "none";
+  ringsProductsDetailContainerInAllSection.style.display = "none";
+});
+
+// Bracelets tab in all
+braceletsProductsInAllSection.addEventListener("click", () => {
+  allProductsInAllSection.style.color = "#D9D9D9";
+  necklaceProductsInAllSection.style.color = "#D9D9D9";
+  braceletsProductsInAllSection.style.color = "#00B8F0";
+  earringsProductsInAllSection.style.color = "#D9D9D9";
+  ringsProductsInAllSection.style.color = "#D9D9D9";
+
+  allProductsInAllSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAllSection.style.backgroundColor = "#D9D9D9";
+  earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAllSection.style.display = "none";
+  necklaceProductsDetailContainerInAllSection.style.display = "none";
+  braceletsProductsDetailContainerInAllSection.style.display = "block";
+  earringsProductsDetailContainerInAllSection.style.display = "none";
+  ringsProductsDetailContainerInAllSection.style.display = "none";
+});
+
+// Earrings tab in all
+earringsProductsInAllSection.addEventListener("click", () => {
+  allProductsInAllSection.style.color = "#D9D9D9";
+  necklaceProductsInAllSection.style.color = "#D9D9D9";
+  braceletsProductsInAllSection.style.color = "#D9D9D9";
+  earringsProductsInAllSection.style.color = "#00B8F0";
+  ringsProductsInAllSection.style.color = "#D9D9D9";
+
+  allProductsInAllSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAllSection.style.backgroundColor = "#D9D9D9";
+  ringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAllSection.style.display = "none";
+  necklaceProductsDetailContainerInAllSection.style.display = "none";
+  braceletsProductsDetailContainerInAllSection.style.display = "none";
+  earringsProductsDetailContainerInAllSection.style.display = "block";
+  ringsProductsDetailContainerInAllSection.style.display = "none";
+});
+
+// Rings tab in all
+ringsProductsInAllSection.addEventListener("click", () => {
+  allProductsInAllSection.style.color = "#D9D9D9";
+  necklaceProductsInAllSection.style.color = "#D9D9D9";
+  braceletsProductsInAllSection.style.color = "#D9D9D9";
+  earringsProductsInAllSection.style.color = "#D9D9D9";
+  ringsProductsInAllSection.style.color = "#00B8F0";
+
+  allProductsInAllSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAllSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAllSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAllSection.style.backgroundColor = "#D9D9D9";
+  allProductsDetailContainerInAllSection.style.display = "none";
+  necklaceProductsDetailContainerInAllSection.style.display = "none";
+  braceletsProductsDetailContainerInAllSection.style.display = "none";
+  earringsProductsDetailContainerInAllSection.style.display = "none";
+  ringsProductsDetailContainerInAllSection.style.display = "block";
+});
+
+
+
+
+// Sub Filter Bar
+// Colombo Section
+// All tab in Colombo
+allProductsInColomboSection.addEventListener("click", () => {
+  allProductsInColomboSection.style.color = "#00B8F0";
+  necklaceProductsInColomboSection.style.color = "#D9D9D9";
+  braceletsProductsInColomboSection.style.color = "#D9D9D9";
+  earringsProductsInColomboSection.style.color = "#D9D9D9";
+  ringsProductsInColomboSection.style.color = "#D9D9D9";
+
+  allProductsInColomboSection.style.backgroundColor = "#D9D9D9";
+  necklaceProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInColomboSection.style.display = "block";
+  necklaceProductsDetailContainerInColomboSection.style.display = "none";
+  braceletsProductsDetailContainerInColomboSection.style.display = "none";
+  earringsProductsDetailContainerInColomboSection.style.display = "none";
+  ringsProductsDetailContainerInColomboSection.style.display = "none";
+});
+
+// Necklace tab in Colombo
+necklaceProductsInColomboSection.addEventListener("click", () => {
+  allProductsInColomboSection.style.color = "#D9D9D9";
+  necklaceProductsInColomboSection.style.color = "#00B8F0";
+  braceletsProductsInColomboSection.style.color = "#D9D9D9";
+  earringsProductsInColomboSection.style.color = "#D9D9D9";
+  ringsProductsInColomboSection.style.color = "#D9D9D9";
+
+  allProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInColomboSection.style.backgroundColor = "#D9D9D9";
+  braceletsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInColomboSection.style.display = "none";
+  necklaceProductsDetailContainerInColomboSection.style.display = "block";
+  braceletsProductsDetailContainerInColomboSection.style.display = "none";
+  earringsProductsDetailContainerInColomboSection.style.display = "none";
+  ringsProductsDetailContainerInColomboSection.style.display = "none";
+});
+
+// Bracelets tab in Colombo
+braceletsProductsInColomboSection.addEventListener("click", () => {
+  allProductsInColomboSection.style.color = "#D9D9D9";
+  necklaceProductsInColomboSection.style.color = "#D9D9D9";
+  braceletsProductsInColomboSection.style.color = "#00B8F0";
+  earringsProductsInColomboSection.style.color = "#D9D9D9";
+  ringsProductsInColomboSection.style.color = "#D9D9D9";
+
+  allProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInColomboSection.style.backgroundColor = "#D9D9D9";
+  earringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInColomboSection.style.display = "none";
+  necklaceProductsDetailContainerInColomboSection.style.display = "none";
+  braceletsProductsDetailContainerInColomboSection.style.display = "block";
+  earringsProductsDetailContainerInColomboSection.style.display = "none";
+  ringsProductsDetailContainerInColomboSection.style.display = "none";
+});
+
+// Earrings tab in Colombo
+earringsProductsInColomboSection.addEventListener("click", () => {
+  allProductsInColomboSection.style.color = "#D9D9D9";
+  necklaceProductsInColomboSection.style.color = "#D9D9D9";
+  braceletsProductsInColomboSection.style.color = "#D9D9D9";
+  earringsProductsInColomboSection.style.color = "#00B8F0";
+  ringsProductsInColomboSection.style.color = "#D9D9D9";
+
+  allProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInColomboSection.style.backgroundColor = "#D9D9D9";
+  ringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInColomboSection.style.display = "none";
+  necklaceProductsDetailContainerInColomboSection.style.display = "none";
+  braceletsProductsDetailContainerInColomboSection.style.display = "none";
+  earringsProductsDetailContainerInColomboSection.style.display = "block";
+  ringsProductsDetailContainerInColomboSection.style.display = "none";
+});
+
+// Rings tab in Colombo
+ringsProductsInColomboSection.addEventListener("click", () => {
+  allProductsInColomboSection.style.color = "#D9D9D9";
+  necklaceProductsInColomboSection.style.color = "#D9D9D9";
+  braceletsProductsInColomboSection.style.color = "#D9D9D9";
+  earringsProductsInColomboSection.style.color = "#D9D9D9";
+  ringsProductsInColomboSection.style.color = "#00B8F0";
+
+  allProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInColomboSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInColomboSection.style.backgroundColor = "#D9D9D9";
+  allProductsDetailContainerInColomboSection.style.display = "none";
+  necklaceProductsDetailContainerInColomboSection.style.display = "none";
+  braceletsProductsDetailContainerInColomboSection.style.display = "none";
+  earringsProductsDetailContainerInColomboSection.style.display = "none";
+  ringsProductsDetailContainerInColomboSection.style.display = "block";
+});
+
+// Ampara Section
+// All tab in Ampara
+allProductsInAmparaSection.addEventListener("click", () => {
+  allProductsInAmparaSection.style.color = "#00B8F0";
+  necklaceProductsInAmparaSection.style.color = "#D9D9D9";
+  braceletsProductsInAmparaSection.style.color = "#D9D9D9";
+  earringsProductsInAmparaSection.style.color = "#D9D9D9";
+  ringsProductsInAmparaSection.style.color = "#D9D9D9";
+
+  allProductsInAmparaSection.style.backgroundColor = "#D9D9D9";
+  necklaceProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAmparaSection.style.display = "block";
+  necklaceProductsDetailContainerInAmparaSection.style.display = "none";
+  braceletsProductsDetailContainerInAmparaSection.style.display = "none";
+  earringsProductsDetailContainerInAmparaSection.style.display = "none";
+  ringsProductsDetailContainerInAmparaSection.style.display = "none";
+});
+
+// Necklace tab in Ampara
+necklaceProductsInAmparaSection.addEventListener("click", () => {
+  allProductsInAmparaSection.style.color = "#D9D9D9";
+  necklaceProductsInAmparaSection.style.color = "#00B8F0";
+  braceletsProductsInAmparaSection.style.color = "#D9D9D9";
+  earringsProductsInAmparaSection.style.color = "#D9D9D9";
+  ringsProductsInAmparaSection.style.color = "#D9D9D9";
+
+  allProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAmparaSection.style.backgroundColor = "#D9D9D9";
+  braceletsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAmparaSection.style.display = "none";
+  necklaceProductsDetailContainerInAmparaSection.style.display = "block";
+  braceletsProductsDetailContainerInAmparaSection.style.display = "none";
+  earringsProductsDetailContainerInAmparaSection.style.display = "none";
+  ringsProductsDetailContainerInAmparaSection.style.display = "none";
+});
+
+// Bracelets tab in Ampara
+braceletsProductsInAmparaSection.addEventListener("click", () => {
+  allProductsInAmparaSection.style.color = "#D9D9D9";
+  necklaceProductsInAmparaSection.style.color = "#D9D9D9";
+  braceletsProductsInAmparaSection.style.color = "#00B8F0";
+  earringsProductsInAmparaSection.style.color = "#D9D9D9";
+  ringsProductsInAmparaSection.style.color = "#D9D9D9";
+
+  allProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAmparaSection.style.backgroundColor = "#D9D9D9";
+  earringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAmparaSection.style.display = "none";
+  necklaceProductsDetailContainerInAmparaSection.style.display = "none";
+  braceletsProductsDetailContainerInAmparaSection.style.display = "block";
+  earringsProductsDetailContainerInAmparaSection.style.display = "none";
+  ringsProductsDetailContainerInAmparaSection.style.display = "none";
+});
+
+// Earrings tab in Ampara
+earringsProductsInAmparaSection.addEventListener("click", () => {
+  allProductsInAmparaSection.style.color = "#D9D9D9";
+  necklaceProductsInAmparaSection.style.color = "#D9D9D9";
+  braceletsProductsInAmparaSection.style.color = "#D9D9D9";
+  earringsProductsInAmparaSection.style.color = "#00B8F0";
+  ringsProductsInAmparaSection.style.color = "#D9D9D9";
+
+  allProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAmparaSection.style.backgroundColor = "#D9D9D9";
+  ringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  allProductsDetailContainerInAmparaSection.style.display = "none";
+  necklaceProductsDetailContainerInAmparaSection.style.display = "none";
+  braceletsProductsDetailContainerInAmparaSection.style.display = "none";
+  earringsProductsDetailContainerInAmparaSection.style.display = "block";
+  ringsProductsDetailContainerInAmparaSection.style.display = "none";
+});
+
+// Rings tab in Ampara
+ringsProductsInAmparaSection.addEventListener("click", () => {
+  allProductsInAmparaSection.style.color = "#D9D9D9";
+  necklaceProductsInAmparaSection.style.color = "#D9D9D9";
+  braceletsProductsInAmparaSection.style.color = "#D9D9D9";
+  earringsProductsInAmparaSection.style.color = "#D9D9D9";
+  ringsProductsInAmparaSection.style.color = "#00B8F0";
+
+  allProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  necklaceProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  braceletsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  earringsProductsInAmparaSection.style.backgroundColor = "#00B8F0";
+  ringsProductsInAmparaSection.style.backgroundColor = "#D9D9D9";
+  allProductsDetailContainerInAmparaSection.style.display = "none";
+  necklaceProductsDetailContainerInAmparaSection.style.display = "none";
+  braceletsProductsDetailContainerInAmparaSection.style.display = "none";
+  earringsProductsDetailContainerInAmparaSection.style.display = "none";
+  ringsProductsDetailContainerInAmparaSection.style.display = "block";
+});
